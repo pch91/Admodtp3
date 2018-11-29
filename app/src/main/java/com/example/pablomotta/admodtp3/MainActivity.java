034d1208736a60a2f9ab4a67d024b3dcb0209bbf
pablo.motta <pch91@bol.com.br>
2018-11-29 21:51:53 -0200

    Initial commit

---
+++ app/src/main/java/com/example/pablomotta/admodtp3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pablomotta.admodtp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-9404483305208909~3507033420");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        mAdView.loadAd(adRequest);
+
+    }
+}
